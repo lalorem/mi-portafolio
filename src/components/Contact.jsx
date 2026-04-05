605c207e2128b6fc6a8a68674b4f8e83f16d8c91
lalorem <correapalaciolorena@gmail.com>
2026-04-04 20:54:29 -0500

    Agregue un nuevo proyecto y organice unas cositas mas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,12 +8,23 @@ export default function Contact() {
           ¿Tienes una idea o proyecto? Me encantaría ayudarte a hacerlo realidad.
         </p>
 
-        <a
-          href="mailto:correapalaciolorena@gmail.com"
-          className="btn-contact"
-        >
-          Enviar correo
-        </a>
+        <div className="btn-group">
+          <a
+            href="mailto:correapalaciolorena@gmail.com"
+            className="btn-contact"
+          >
+            Enviar correo
+          </a>
+
+          <a
+            href="https://wa.me/573024446708?text=Hola%20Star!%20Estoy%20interesado%20en%20que%20me%20ayudes%20con%20una%20página%20web."
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-contact"
+          >
+            Enviar mensaje por WhatsApp
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,28 @@
+// Projects.jsx
 const projectsData = [
   {
     title: "Tienda Sanrio",
     description:
       "Es una tienda web donde los usuarios pueden ver productos, explorar categorías y agregarlos a un carrito. Está pensada para que la navegación sea fácil y tenga un diseño bonito y agradable para el usuario.",
     image: "/sanriostar.jpg",
-    tags: ["HTML", "CSS", "PHP", "JavaScript", "React", "Laravel", "MVC"]
+    tags: ["HTML", "CSS", "PHP", "JavaScript", "React", "Laravel", "MVC"],
+    repo: "https://github.com/isa1227/sanrio-star"
   },
   {
     title: "Qvision",
     description:
       "Es un proyecto realizado como prueba técnica, en el que se replicó una página web real. Se enfocó en recrear su diseño y funcionamiento, buscando que se vea y se comporte de forma similar al original.",
     image: "/qvision.png",
-    tags: ["Laravel", "PHP", "CSS", "HTML", "MySQL", "JavaScript"]
+    tags: ["Laravel", "PHP", "CSS", "HTML", "MySQL", "JavaScript"],
+    repo: "https://github.com/lalorem/qvision"
+  },
+  {
+    title: "GL Aires y Refrigeración",
+    description:
+      "Es un proyecto web desarrollado para una empresa de aires acondicionados y refrigeración. Permite a los clientes conocer los servicios ofrecidos y contactar fácilmente con la empresa.",
+    image: "/Logo.png",
+    tags: ["HTML", "CSS", "Script"],
+    repo: "https://github.com/lalorem/GL-Aires-y-Refrigeracion"
   }
 ];
 
@@ -22,47 +33,28 @@ export default function Projects() {
       <p>Algunos proyectos en los que he trabajado</p>
 
       <div className="grid">
+        {projectsData.map((project, index) => (
+          <div key={index} className="project-card slide-up">
+            <div className="img">
+              <img src={project.image} alt={project.title} />
+            </div>
 
-        {/* 🔹 Proyecto 1 */}
-        <div className="project-card slide-up">
-          <div className="img">
-            <img src={projectsData[0].image} alt={projectsData[0].title} />
+            <div className="project-content">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+
+              <div className="tags">
+                {project.tags.map((tag) => (
+                  <span key={tag}>{tag}</span>
+                ))}
+              </div>
+
+              <button onClick={() => window.open(project.repo, "_blank")}>
+                Ver repositorio
+              </button>
+            </div>
           </div>
-
-          <h3>{projectsData[0].title}</h3>
-          <p>{projectsData[0].description}</p>
-
-          <div className="tags">
-            {projectsData[0].tags.map((tag) => (
-              <span key={tag}>{tag}</span>
-            ))}
-          </div>
-
-          <button onClick={() => window.open("https://github.com/isa1227/sanrio-star", "_blank")}>
-            Ver repositorio
-          </button>
-        </div>
-
-        {/* 🔹 Proyecto 2 */}
-        <div className="project-card slide-up">
-          <div className="img">
-            <img src={projectsData[1].image} alt={projectsData[1].title} />
-          </div>
-
-          <h3>{projectsData[1].title}</h3>
-          <p>{projectsData[1].description}</p>
-
-          <div className="tags">
-            {projectsData[1].tags.map((tag) => (
-              <span key={tag}>{tag}</span>
-            ))}
-          </div>
-
-          <button onClick={() => window.open("https://github.com/lalorem/qvision", "_blank")}>
-            Ver repositorio
-          </button>
-        </div>
-
+        ))}
       </div>
     </section>
   );
